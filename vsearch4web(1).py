17aa1ca22f9d1283c1b9a7d340680daf2938ab1d
kkrrystal2 <m15915778956@163.com>
2017-06-01 10:50:15 +0800

    表头

---
+++ vsearch4web(1).py
@@ -0,0 +1,57 @@
+ # -*- coding: utf-8 -*- 
+from flask import Flask, render_template, request, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    with open('vsearch.log', 'a', encoding='utf8') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    color = request.form['user_color']	#从entry.html模版（输入）取到变数名称user_color的值，存放在color这变数下
+    title = '以下是您的结果：'
+    results = search4letters(phrase, letters)
+    log_request(request, results)
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,
+                           the_color=color,		#flask.render_template 函数把results.html模版（输出），其中模版中the_color的值，用color这变数之值
+                           )
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
+    return render_template('entry.html',
+                           the_title='欢迎来到网上 历史上的今天查询！')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('表单内容', '访问者IP', '浏览器', '运行结果')
+    return render_template('viewlog.html',
+                           the_title='查看日志',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
